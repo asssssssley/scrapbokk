4d8c15d7c982b8b53a9d32b5827b4c614c1c39f8
Ashley Luong <ashleyluong18@gmail.com>
2024-12-28 03:51:58 +1100

    fixed auth and improved UI of signin

--- client/src/components/Signin/Signin.jsx
+++ client/src/components/Signin/Signin.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Typography, Button } from "@mui/material";
+import { Typography, Button, Box } from "@mui/material";
 import useAuth from "../../context/useAuth";
 
 const Signin = ({ type }) => {
@@ -20,7 +20,7 @@ const Signin = ({ type }) => {
       // render Google Sign-In button
       window.google.accounts.id.renderButton(
         document.getElementById("signinButton"),
-        { theme: "outline", size: "large" }
+        {}
       );
     }
   }, []);
@@ -36,11 +36,12 @@ const Signin = ({ type }) => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ token }),
+        credentials: "include",
       });
     
       const data = await res.json();
       if (res.ok) {
-        login(data.token);
+        login();
         navigate("/dashboard");
       } else {
         console.error("Backend error:", data.error);
@@ -51,20 +52,16 @@ const Signin = ({ type }) => {
   };
   
   return (
-    <div style={{ display: "grid", gridTemplateColumns: "70% 30%", minHeight: "100vh", height: "auto", width: "100vw" }}>
-      <div style={{ backgroundColor: type === "login" ? "#5C6B42" : "#93A072", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <img
-          src="/duck_beige.png"
-          alt="logo"
-          style={{ height: "auto", paddingLeft: "10px", width: "80px", position: "relative", top: "-10px", left: "-5px" }}
-        />
-        {type === "login" ?  (
-          <Typography variant="h6" sx={{ color: "#F4EED8" }}>... hurry up and log in</Typography>
-        ) : (
-          <Typography variant="h6" sx={{ color: "#F4EED8" }}>... hurry up and sign up</Typography>
-        )}
-      </div>
-      <div style={{ backgroundColor: type === "login" ? "#93A072" : "#5C6B42", display: "flex", justifyContent: "center", alignContent: "center" }} >
+    <Box 
+      sx={{ 
+        display: "grid", 
+        gridTemplateColumns: { xs: "1fr", sm: "50% 50%", md: "70% 30%" }, 
+        minHeight: "100vh",
+        height: "auto", 
+        width: "100vw",
+      }}
+    >
+      <div style={{ backgroundColor: type === "login" ? "#5C6B42" : "#93A072" }}>
         <div style={{ position: "absolute", right: "0" }}>
           <Link to="/" style={{ textDecoration: "none" }}>
             <Button 
@@ -103,8 +100,8 @@ const Signin = ({ type }) => {
                 margin: { xs: 1, md: 2 }, 
                 marginLeft: 1, 
                 borderRadius: "20px", 
-                backgroundColor: type === "login" ? "#5C6B42" : "#93A072", 
-                border: `2px solid ${type === "login" ? "#5C6B42" : "#93A072"}`,
+                backgroundColor: { xs: type === "login" ? "#93A072" : "#5C6B42", sm: type === "login" ? "#5C6B42" : "#93A072" }, 
+                border: { xs: `2px solid ${type === "login" ? "#93A072" : "#5C6B42"}`, sm: `2px solid ${type === "login" ? "#5C6B42" : "#93A072"}` },
                 color: "#F4EED8", 
                 fontWeight: "bold", 
                 padding: 1, 
@@ -128,9 +125,38 @@ const Signin = ({ type }) => {
             </Button>
           </Link>
         </div>
+        <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
+        <style>
+            {`
+              @keyframes walk {
+                0% {
+                  transform: rotate(-5deg);  
+                }
+                50% {
+                  transform: rotate(5deg);   
+                }
+                100% {
+                  transform: rotate(-5deg);  
+                }
+              } 
+            `}
+          </style>
+          <img
+            src="/duck_beige.png"
+            alt="logo"
+            style={{ height: "auto", paddingLeft: "10px", width: "80px", position: "relative", top: "-10px", left: "-5px", animation: "walk 1s ease-in-out infinite" }}
+          />
+          {type === "login" ?  (
+            <Typography variant="h6" sx={{ color: "#F4EED8" }}>... hurry up and log in</Typography>
+          ) : (
+            <Typography variant="h6" sx={{ color: "#F4EED8" }}>... hurry up and sign up</Typography>
+          )}
+        </Box>
+      </div>
+      <div style={{ backgroundColor: type === "login" ? "#93A072" : "#5C6B42", display: "flex", justifyContent: "center", alignContent: "center" }} >
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }} id="signinButton"></div>
       </div>
-    </div>
+    </Box>
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -2,16 +2,34 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const { signin } = require("./src/auth");
+const { signin, checkAuth, logout } = require("./src/auth");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  console.log(`Received ${req.method} request to ${req.url}`);
+  next();
+});
+
 // sign in route
 app.post("/signin", signin);
+
+// check auth route
+app.get("/checkAuth", checkAuth, (req, res) => {
+  res.json({ message: "Authenticated", user: req.user });
+});
+
+// logout route
+app.get("/logout", logout);
 
 const PORT = 5000;
 app.listen(PORT, () => {
