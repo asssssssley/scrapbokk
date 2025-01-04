94d89d00761e706b2bee2d3b8a21526c8b6abd8a
Rokika Kh <rokika.kh1@gmail.com>
2025-01-04 18:00:32 +1100

    refactored dashboard

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,47 +1,21 @@
 import React, { useContext, useState } from "react";
-import DarkModeContext from "../../context/DarkModeContext";
 import AuthContext from "../../context/AuthContext";
-import { ThemeProvider, createTheme, Button, CssBaseline, Typography } from '@mui/material';
-import LightModeIcon from '@mui/icons-material/LightMode';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import CreateScrapbookModal from './CreateScrapbookModal';
-import ScrapbookDisplay from './ScrapbookDisplay';
+import { ThemeProvider, CssBaseline, Typography, Button } from "@mui/material";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import CreateScrapbookModal from "./CreateScrapbookModal";
+import ScrapbookDisplay from "./ScrapbookDisplay";
+import DarkModeButton from "../Theme/DarkModeButton";
+import { darkTheme, lightTheme } from "../Theme/theme";
+import DarkModeContext from "../../context/DarkModeContext";
 
 const Dashboard = () => {
   const [open, setOpen] = useState(false);
-  const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
+  const { darkMode } = useContext(DarkModeContext);
   const { logout } = useContext(AuthContext);
-
-  const darkTheme = createTheme({
-    palette: {
-      mode: 'dark',
-      background: { default: '#323232' },
-      text: { primary: '#93a174' },
-    },
-  });
-
-  const lightTheme = createTheme({
-    palette: {
-      mode: 'light',
-      background: { default: '#f6eed6' },
-      text: { primary: '#5d6a43' },
-    },
-  });
 
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
-
-  const getButtonStyles = (darkMode, darkTheme, lightTheme) => ({
-    position: "fixed",
-    padding: "5px",
-    transition: "background-color 0.3s",
-    borderRadius: "30px",
-    fontSize: { xs: "0.70rem", sm: "1rem", md: "1.3rem" },
-    backgroundColor: darkMode ? lightTheme.palette.background.default : darkTheme.palette.background.default,
-    color: darkMode ? lightTheme.palette.text.primary : darkTheme.palette.text.primary,
-  });
 
   const getTopButtonStyles = (darkMode, darkTheme, lightTheme) => ({
     position: "absolute",
@@ -49,40 +23,48 @@ const Dashboard = () => {
     borderRadius: "30px",
     fontSize: { xs: "0.6rem", sm: "0.8rem", md: "1rem" },
     padding: { xs: "0.7rem", sm: "1rem", md: "1.2rem" },
-    backgroundColor: darkMode ? darkTheme.palette.text.primary : lightTheme.palette.text.primary,
-    color: darkMode ? darkTheme.palette.background.default : lightTheme.palette.background.default,
+    backgroundColor: darkMode
+      ? darkTheme.palette.text.primary
+      : lightTheme.palette.text.primary,
+    color: darkMode
+      ? darkTheme.palette.background.default
+      : lightTheme.palette.background.default,
     "&:hover": {
-      backgroundColor: darkMode ? lightTheme.palette.text.primary : darkTheme.palette.text.primary,
+      backgroundColor: darkMode
+        ? lightTheme.palette.text.primary
+        : darkTheme.palette.text.primary,
     },
   });
 
   const scrapbooks = [
-    { title: 'London', pages: '12', img: 'https://via.placeholder.com/150/0000FF/808080?Text=PAKAINFO.com' },
-    { title: 'Sydney', pages: '10', img: 'https://i.imgur.com/CzXTtJV.jpg' },
-    { title: 'Rome', pages: '7', img: 'https://i.imgur.com/OB0y6MR.jpg' },
+    { title: "London", pages: "12", img: "https://via.placeholder.com/150/0000FF/808080?Text=PAKAINFO.com" },
+    { title: "Sydney", pages: "10", img: "https://i.imgur.com/CzXTtJV.jpg" },
+    { title: "Rome", pages: "7", img: "" },
+    { title: "Sydney", pages: "10", img: "https://i.imgur.com/CzXTtJV.jpg" },
+    { title: "Rome", pages: "7", img: "https://i.imgur.com/OB0y6MR.jpg" },
   ];
 
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <CssBaseline />
 
-      <Button
-        onClick={toggleDarkMode}
-        sx={{
-          ...getButtonStyles(darkMode, darkTheme, lightTheme),
-          bottom: "20px",
-          right: "20px",
-        }}
-      >
-        {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
-      </Button>
+      <DarkModeButton />
 
       <Button
         onClick={scrollToTop}
         sx={{
-          ...getButtonStyles(darkMode, darkTheme, lightTheme),
+          position: "fixed",
           bottom: "20px",
           left: "20px",
+          padding: "5px",
+          borderRadius: "30px",
+          transition: "background-color 0.3s",
+          backgroundColor: darkMode
+            ? lightTheme.palette.background.default
+            : darkTheme.palette.background.default,
+          color: darkMode
+            ? lightTheme.palette.text.primary
+            : darkTheme.palette.text.primary,
         }}
       >
         <KeyboardArrowUpIcon />
--- client/src/components/Dashboard/ScrapbookDisplay.jsx
+++ client/src/components/Dashboard/ScrapbookDisplay.jsx
@@ -6,50 +6,59 @@ const ScrapbookDisplay = ({ scrapbooks }) => {
   return (
     <Box
       sx={{
-        display: 'grid',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
         width: '100%',
-        margin: 'auto',
-        gridTemplateColumns: {
-          xs: 'repeat(2, 1fr)',
-          sm: 'repeat(3, 1fr)',
-          md: 'repeat(4, 1fr)'
-        },
-        gap: { xs: '1em', sm: '2em', md: '4em' },
-        paddingY: { xs: '5rem', sm: '7rem', md: '9rem' },
+        paddingTop: { xs: '5rem', sm: '7rem', md: '9rem' },
       }}
     >
-      {scrapbooks.map((scrapbook, index) => (
-        <Card
-          key={index}
-          sx={{
-            width: { xs: 200, sm: 250, md: 300 },
-            height: { xs: 200, sm: 250, md: 300 },
-            padding: 2,
-            borderRadius: 5,
-            boxShadow: 3,
-          }}
-        >
-          <CardMedia
-            component="img"
+      <Box
+        sx={{
+          display: 'grid',
+          width: '100%',
+          maxWidth: '1200px',
+          gridTemplateColumns: {
+            xs: 'repeat(2, 1fr)',
+            sm: 'repeat(3, 1fr)',
+            md: 'repeat(4, 1fr)',
+          },
+          gap: { xs: '1em', sm: '2em', md: '4em' },
+        }}
+      >
+        {scrapbooks.map((scrapbook, index) => (
+          <Card
+            key={index}
             sx={{
+              width: { xs: 200, sm: 250, md: 300 },
+              height: { xs: 200, sm: 250, md: 300 },
+              padding: 2,
               borderRadius: 5,
-              height: { xs: 100, sm: 150, md: 200 },
+              boxShadow: 3,
             }}
-            image={scrapbook.img || thumbnailImg}
-            alt="scrapbook-thumbnail"
-          />
-          <CardContent sx={{ padding: 1 }}>
-            <Typography variant="h6" component="div">
-              {scrapbook.title.length > 18
-                ? `${scrapbook.title.slice(0, 18)}...`
-                : scrapbook.title}
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              {scrapbook.pages} pages
-            </Typography>
-          </CardContent>
-        </Card>
-      ))}
+          >
+            <CardMedia
+              component="img"
+              sx={{
+                borderRadius: 5,
+                height: { xs: 100, sm: 150, md: 200 },
+              }}
+              image={scrapbook.img || thumbnailImg}
+              alt="scrapbook-thumbnail"
+            />
+            <CardContent sx={{ padding: 1 }}>
+              <Typography variant="h6" component="div">
+                {scrapbook.title.length > 18
+                  ? `${scrapbook.title.slice(0, 18)}...`
+                  : scrapbook.title}
+              </Typography>
+              <Typography variant="body1" color="text.secondary">
+                {scrapbook.pages} pages
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </Box>
     </Box>
   );
 };
